3bd41c0635ecf719fdbb2cdbaec10267bdfe5e4b
Steve-Clarke1 <84132008+Steve-Clarke1@users.noreply.github.com>
2022-03-10 09:50:13 +0000

    Create main.r

---
+++ main.r
@@ -0,0 +1,11 @@
+# Import Packages
+# --------------
+source("rscripts/0_packages.r")
+
+# Import UDFs
+# --------------
+source("rscripts/1_udfs.r")
+
+# Data Import and Cleansing
+# ----------------
+source("2_dataimport.r")
